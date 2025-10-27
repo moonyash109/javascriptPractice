e26f0bbb6db356cc10f795ac192f260d3304d3ad
moonyash109 <112071496+moonyash109@users.noreply.github.com>
2025-10-27 17:16:04 +0000

    learn datatypes and varibales

---
+++ 01_js/01_datatypes.js
@@ -0,0 +1,13 @@
+// number
+// bigInt
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+let name = "yash";
+let age = 21;
+let islogIn = true;
+
+console.table([name,age,islogIn]);
---
+++ 01_js/01_variables.js
@@ -0,0 +1,8 @@
+const id = 12345;
+var name = "yash moon";
+let pass = "12345";
+city = "Pune";
+// id = 1254;  constant can't be changed
+console.log(id);
+
+console.table([id,name,pass,city]);
